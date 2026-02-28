3793d9e0e4d55d19f1461802888e935e4aafcd0c
aniket-padyal <aniketpadyal07@gmail.com>
2026-02-28 15:52:29 +0530

    move code to TS

---
+++ index.ts
@@ -0,0 +1,47 @@
+const menu=[
+    {name:"Margherita", price:8},
+    {name:"Pepperoni", price:10},
+    {name:"Hawaiian", price:10},
+    {name:"Veggie", price:7},
+]
+
+let nextOrderId=1;
+let cashInRegister = 100;
+const orderQueue = [];
+
+function addNewPizza(pizzaObj) {
+    menu.push(pizzaObj)
+}
+
+function placeOrder(pizzaName) {
+    const selectedPizza = menu.find(item => item.name === pizzaName);
+    cashInRegister += selectedPizza.price;
+    let newOrder = { pizza: selectedPizza, status: "ordered", id: nextOrderId++ };
+    orderQueue.push(newOrder);
+    return newOrder;
+}
+
+function completeOrder(orderId) {
+    const order = orderQueue.find(order => order.id === orderId);
+    order.status = "completed";
+    return completeOrder;
+}
+
+addNewPizza({name:"Golden Corn", price:12});
+addNewPizza({name:"Onion", price:1});
+addNewPizza({name:"Paneer", price:2});
+
+console.log(menu);
+
+placeOrder("Veggie");
+placeOrder("Onion");
+placeOrder("Paneer");
+placeOrder("Golden Corn");
+
+console.log(orderQueue);
+
+completeOrder(1);
+completeOrder(4);
+completeOrder(2);
+
+console.log(orderQueue);
